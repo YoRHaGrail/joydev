b611b795ec405e9b6ee0d5c8e7d44c25dfb4a1f6
Kaleb Turner <kaleb@netgen.co.za>
2022-07-12 23:21:37 +0200

    updated themes in tailwind config

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,2 +1,12 @@
-const sidebar = () => {};
+const sidebar = () => {
+  return (
+    <div className="fixed top-0 h-screen w-15 m-0 flex flex-col">
+      <i>test line 1</i>
+      <i>test line 2</i>
+      <i>test line 3</i>
+      <i>test line 4</i>
+      <i>test line 5</i>
+    </div>
+  );
+};
 export default sidebar;
